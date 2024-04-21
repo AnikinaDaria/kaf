edd410cf9941cf1a5043937f91ec0b8ff3fe5115
AnikinaDaria <dasha.anik.al@gmail.com>
2024-04-21 15:04:34 +0400

    commit

---
+++ src/main/java/com/kaf/kafkademo/kafka/kafkaConsumer.java
@@ -0,0 +1,14 @@
+package com.kaf.kafkademo.kafka;
+
+import lombok.AllArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class kafkaConsumer {
+    @KafkaListener(topics = "kaf", groupId = "my_consumer")
+    public void listen(String message) {
+        
+        System.out.println("MESSAGE = " + message);
+    }
+}
---
+++ src/test/java/com/kaf/kafkademo/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaf.kafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
